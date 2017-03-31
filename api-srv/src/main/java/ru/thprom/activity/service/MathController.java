f9a9545717a0b69cc477d0ab2979303d026d11a1
Камишкерцев Александр Вячеславович <akamishkertsev@at-consulting.ru>
2017-03-31 15:24:08 +0300

    first commit

---
+++ api-srv/src/main/java/ru/thprom/activity/service/MathController.java
@@ -0,0 +1,40 @@
+package ru.thprom.activity.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class MathController {
+
+    AtomicInteger counter = new AtomicInteger();
+    volatile long startTime = 0L;
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping("/math")
+    public long call(@RequestParam(value="n", defaultValue="12") int value) {
+        long result = value * value;
+
+        int counterValue = counter.incrementAndGet();
+
+        if (counterValue == 1) {
+            startTime = System.currentTimeMillis();
+        }
+
+        if (counterValue % 1000 == 0) {
+            int taskPerSeconds = (int) (counterValue / ((float) (System.currentTimeMillis() - startTime) / 1000));
+            log.info("Counter = {}, task per seconds = {}", counterValue, taskPerSeconds);
+
+        }
+
+        return result;
+    }
+
+}
